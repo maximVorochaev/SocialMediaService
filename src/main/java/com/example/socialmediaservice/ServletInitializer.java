5d398c0754d14f6054fb72d54020dc9bd35f382e
maxim <maxim.vorochaev@mail.ru>
2020-01-14 11:52:42 +0300

    added the ability to deploy Spring Boot application to tomcat

---
+++ src/main/java/com/example/socialmediaservice/ServletInitializer.java
@@ -0,0 +1,9 @@
+package com.example.socialmediaservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ServletInitializer extends SpringBootServletInitializer {
+
+}
